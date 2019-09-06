20150f1768cf194a58945492ae6f0942df5c4cfd
slzll <1259654637@qq.com>
2019-09-06 17:13:52 +0800

    修改SavedSuggestions为StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/randomWords.dart';
-// import 'randomWords.dart';
 
 void main() => runApp(MyApp());
 
--- lib/savedSuggestions.dart
+++ lib/savedSuggestions.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 
-class SavedSuggestions extends StatefulWidget {
+class SavedSuggestions extends StatelessWidget {
   final saved;
   final biggerFont;
-  SavedSuggestions({Key key, this.saved, this.biggerFont}) : super(key: key);
+  SavedSuggestions({this.saved, this.biggerFont});
 
-  @override
-  _SavedSuggestionsState createState() => new _SavedSuggestionsState();
-}
-
-class _SavedSuggestionsState extends State<SavedSuggestions> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -17,22 +12,59 @@ class _SavedSuggestionsState extends State<SavedSuggestions> {
         title: new Text("Saved Suggestions"),
       ),
       body: new ListView(
-        children: divided(),
+        children: divided(context),
       ),
     );
   }
 
-  List<Widget> divided() {
+  List<Widget> divided(ctx) {
     return ListTile.divideTiles(
-      context: context,
-      tiles: widget.saved.map<Widget>((pair) {
+      context: ctx,
+      tiles: saved.map<Widget>((pair) {
         return new ListTile(
           title: new Text(
             pair.asPascalCase,
-            style: widget.biggerFont,
+            style: biggerFont,
           ),
         );
       }),
     ).toList();
   }
 }
+
+// class SavedSuggestions extends StatefulWidget {
+//   final saved;
+//   final biggerFont;
+//   SavedSuggestions({Key key, this.saved, this.biggerFont}) : super(key: key);
+
+//   @override
+//   _SavedSuggestionsState createState() => new _SavedSuggestionsState();
+// }
+
+// class _SavedSuggestionsState extends State<SavedSuggestions> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: new Text("Saved Suggestions"),
+//       ),
+//       body: new ListView(
+//         children: divided(),
+//       ),
+//     );
+//   }
+
+//   List<Widget> divided() {
+//     return ListTile.divideTiles(
+//       context: context,
+//       tiles: widget.saved.map<Widget>((pair) {
+//         return new ListTile(
+//           title: new Text(
+//             pair.asPascalCase,
+//             style: widget.biggerFont,
+//           ),
+//         );
+//       }),
+//     ).toList();
+//   }
+// }
